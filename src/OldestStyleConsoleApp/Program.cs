4850973dabad3a7386641ba771fd589cbcf9f474
Louis DeJardin <lodejard@microsoft.com>
2019-04-30 14:23:13 -0700

    Adding example of csproj with packages.config

---
+++ src/OldestStyleConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace OldestStyleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webHost = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:9292")
+                .UseStartup<Startup>()
+                .Build();
+
+            webHost.Run();
+        }
+    }
+}
